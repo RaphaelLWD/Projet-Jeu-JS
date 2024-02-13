dc523dcf989d85cf97bae3dc5e66cf0b95239005
Raph <raphael.lisenborg.simplon@gmail.com>
2024-02-13 16:10:25 +0100

    Phase finale d'approche

--- Classe/circuit.js
+++ Classe/circuit.js
@@ -1,15 +1,21 @@
 export default class Circuit {
 
     #typesRoutes = ["./medias/NormalRoad.jpg", "./medias/BoueuseRoad.jpg", "./medias/MagmaRoad.jpg", "./medias/WaterRoad.jpg", "./medias/SnowRoad.jpg"];
-    constructor() {
-        this.routeAleatoire();
+    constructor(index) {
+        this.routeAleatoire(index);
     }
-    routeAleatoire() {
-        let routeAleatoire = Math.floor(Math.random() * this.#typesRoutes.length);
-        document.querySelector('.routeGoudron').src = this.#typesRoutes[routeAleatoire];
+    routeAleatoire(currentIndex) {
+        if (currentIndex <= this.#typesRoutes.length - 1) {
+            document.querySelector('.routeGoudron').src = this.#typesRoutes[currentIndex];
+        } else {
+            document.querySelector('.routeGoudron').src = "./medias/GameOver.jpg";
+        }
+
     }
+
     get boutonEnRoute() {
         return document.querySelector('.enRoute');
     }
+
 
 }   
--- script.js
+++ script.js
@@ -1,54 +1,174 @@
 import Circuit from "./Classe/circuit.js";
 
-let circuit = new Circuit();
+// ------ Mon switch de circuits ------//
 
-let trajet = document.querySelector('.trajetVehicule');
+let circuit = new Circuit(0);
+let currentIndex = 0;
 
-function monTrajet() {
-    trajet.addEventListener("click", () => {
-        let start = Date.now();
+// -------------- Trajet + Score -------------------//
 
-        let timer = setInterval(function () {
-            let timePassed = Date.now() - start;
+/* Trajet GOUDRON (à modifier au niveau des valeurs des attributs pour faire correspondre à la route en goudron cf.doc papier)*/
+document.getElementById('startButton').addEventListener('click', function () {
 
-            trajet.style.left = timePassed / 5 + 'px';
+    let selectionType = document.querySelector('input[name="type"]:checked');
+    let selectionForme = document.querySelector('input[name="forme"]:checked');
+    let selectionMatiere = document.querySelector('input[name="matiere"]:checked');
 
-            if (timePassed > 5200) clearInterval(timer);
-
-        }, 20);
-    });
-}
-
-
-let attributs = ["lisse", "à clou", "à aube", "rond", "carre", "étoile", "bois", "metal", "caoutchouc"];
-let scoreParAttributs = ["mauvais" = 0, "bon" = 50, "excellent" = 100];
-
-
-
-function vitesselente() {
-    for (i = 0; i <= 100; i++) {
-    };
-    trajet = vitesselente();
-}
-function vitesseNormale() {
-    for (i = 150; i <= 250; i++) {
-    };
-    trajet = vitesseNormale();
-}
-function vitesseOptimal() {
-    for (i = 300; i++;) {
-    };
-    trajet = vitesseOptimal();
-}
-function vitesseSelonScore() {
-    switch (score) {
-        case vitesselente():
-
-            break;
-
-        default:
-            break;
+    if (!selectionType || !selectionForme || !selectionMatiere) {
+        alert("Veuillez sélectionner un type, une forme et un matériau.");
+        return;
     }
+
+    let typeValeur = selectionType.value;
+    let formeValeur = selectionForme.value;
+    let matiereValeur = selectionMatiere.value;
+
+    let typeSpeed;
+    let typeScore;
+    if (typeValeur === "lisse") {
+        typeSpeed = 1; // vitesse rapide
+        typeScore = 100; // score pour vitesse rapide
+    } else if (typeValeur === "à clou") {
+        typeSpeed = 2; // vitesse normale
+        typeScore = 50; // score pour vitesse normale
+    } else if (typeValeur === "à aube") {
+        typeSpeed = 3; // vitesse lente
+        typeScore = 0; // score pour vitesse lente
+    }
+
+    let formeSpeed;
+    let formeScore;
+    if (formeValeur === "rond") {
+        formeSpeed = 1;
+        formeScore = 100;
+    } else if (formeValeur === "carre") {
+        formeSpeed = 2;
+        formeScore = 50;
+    } else if (formeValeur === "etoile") {
+        formeSpeed = 3;
+        formeScore = 0;
+    }
+
+    let matiereSpeed;
+    let matiereScore;
+    if (matiereValeur === "caoutchouc") {
+        matiereSpeed = 1;
+        matiereScore = 100;
+    } else if (matiereValeur === "bois") {
+        matiereSpeed = 2;
+        matiereScore = 50;
+    } else if (matiereValeur === "metal") {
+        matiereSpeed = 3;
+        matiereScore = 0;
+    }
+
+    let speed = typeSpeed + formeSpeed + matiereSpeed;
+    let score = typeScore + formeScore + matiereScore;
+
+    let vehicule = document.querySelector('.trajetVehicule');
+    let container = document.querySelector('.trajet');
+    let containerWidth = container.offsetWidth;
+    let newPosition = containerWidth - vehicule.offsetWidth;
+
+    vehicule.style.left = newPosition + 'px';
+    vehicule.style.transition = 'left ' + speed + 's linear';
+
+    // Fin du trajet : Affichage du score
+    setTimeout(function () {
+        // Affichage du score
+        document.getElementById('score').innerText = "Score: " + score;
+    }, speed * 1000); // Le délai correspond à la durée du trajet
+});
+
+// --------- Bouton qui passe à au circuit suivant --------//
+
+let circuitSuivant = document.querySelector('#nextButton');
+let repositionneVehicule = document.querySelector('.trajetVehicule');
+circuitSuivant.addEventListener("click", () => {
+    // joue la fonction qui permet de passer au circuit suivant //
+
+    circuit.routeAleatoire(currentIndex);
+    currentIndex++;
+    repositionneVehicule.style.left = 0;;
+
+})
+// Trajet Boueux // 
+// Trajet Enneigé //
+// TRajet Eau // 
+// Trajet Magma // 
+
+//----- Ajout bande-son -------//
+
+let audio = document.getElementById('audio');
+let joueActuel = false;
+
+// Lancer ou arrêter la musique au clic sur le bouton
+document.getElementById('toggleButton').addEventListener('click', function () {
+    if (!joueActuel) {
+        play();
+    } else {
+        stop();
+    }
+});
+
+// Fonction pour lancer la musique
+function play() {
+    audio.play();
+    joueActuel = true;
+    document.getElementById('icon').src = './medias/volume-eleve.png'; // Changer l'icône en pause
 }
 
+// Fonction pour arrêter la musique
+function stop() {
+    audio.pause();
+    audio.currentTime = 0; // Rembobiner au début
+    joueActuel = false;
+    document.getElementById('icon').src = 'muet.png'; // Changer l'icône en play
+}
+// ------- Nom de chaque route qui apparait en fonction de sa route ---------//
 
+// ------- Pop-up -------//
+// ------------------ Switch Acceuil - Jeu + Play/Display Règles---------------------//
+let bouton = document.querySelector('#boutonAcc');
+let popupPlay = document.querySelector('.popUp');
+let jeuPlay = document.querySelector('.jeu');
+let reglesBouton = document.querySelector('#regles');
+let reglesPlay = document.querySelector('.reglesDeJeu');
+bouton.addEventListener("click", displayAcceuil);
+reglesBouton.addEventListener("click", playRegles);
+
+function displayAcceuil() {
+    popupPlay.classList.remove("Play");
+    popupPlay.classList.add("Display");
+    jeuPlay.classList.add('Play')
+    jeuPlay.classList.remove('Display')
+}
+function playRegles() {
+    reglesPlay.classList.toggle("Display")
+    reglesPlay.classList.toggle("Play")
+}
+
+// -------------------- Pop des règles ------------------//
+
+
+
+reglesBouton.addEventListener("click", () => {
+    reglesPlay.innerText = `         Bienvenue sur SMARTTRUCK 
+
+    Vous êtes un Roadtriper à la recherche des plus beaux paysages sur Terre.
+    Equipez de votre véhicule, vous vous apprêtez à arpenter les routes les plus impraticables!
+    
+    ' Ha ha je me ris du danger, rien ne m'arrêtera !!' dîtes vous à voix haute.
+    
+    Réflechissez aux attributs à ajouter à votre véhicule sur chaque circuits pour franchir
+    les 5 terrains de manière la plus optimal.
+
+    Chaque circuit peut vous octroyer entre 0 et 300 points.
+    Le score maximal possible est de 1000 points.
+    
+    Vos choix seront décisifs quand au score final!
+    
+    Confrontez votre logique aux autres Roadtripers, que le meilleur gagne!`
+    reglesPlay.style.margin = "10px";
+    reglesPlay.style.textAlign = "center";
+});
