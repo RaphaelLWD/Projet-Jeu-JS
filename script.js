de4f57947f9cdacf0aa73babd2df808d5483d496
Raph <raphael.lisenborg.simplon@gmail.com>
2024-02-13 17:03:07 +0100

    Vrai vrai finale

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ let currentIndex = 0;
 
 // -------------- Trajet + Score -------------------//
 function ChangeNameRoad() {
-    switch (document.querySelector('.typeCircuit').src) {
+    switch (document.querySelector('.routeGoudron').src) {
         case "https://soft-youtiao-97d35b.netlify.app/medias/NormalRoad.jpg":
             document.querySelector('.typeCircuit').innerText = "Route Goudron";
             break;
